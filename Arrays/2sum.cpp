7c039b9c49f63318e55e769c2c91058cb0b8aa3d
vip23anchib <barman23vipanchi@gmail.com>
2025-12-22 14:32:22 +0000

    2 sum optimal approach

--- Arrays/2sum.cpp
+++ Arrays/2sum.cpp
@@ -13,6 +13,7 @@ map[nums[i]] = i*/
 #include <iostream>
 #include <vector>
 #include<unordered_map>
+#include<algorithm>
 using namespace std;
 
 void two_sum1(vector<int> &arr,int num){// brute force O(n^2)
@@ -40,8 +41,38 @@ void two_sum2(vector<int>& arr, int target) {// using hashing O(n) time and O(n)
             return;
         }
 
-        // if not present in map yet thenstore current value with its index
+        // if not present in map yet then store current value with its index
         mp[arr[i]] = i;
+    }
+
+    cout << "no" << endl;
+}
+
+void two_sum3(vector<int>& arr, int target) {// two pointer approach O(nlogn) due to sorting and O(1) space
+    vector<pair<int, int>> vec; // to store value and its original index
+
+    for (int i = 0; i < arr.size(); i++) {
+        vec.push_back({arr[i], i});
+    }
+
+    // sort based on values
+    sort(vec.begin(), vec.end());
+
+    int left = 0;
+    int right = vec.size() - 1;
+
+    while (left < right) {
+        int current_sum = vec[left].first + vec[right].first;
+
+        if (current_sum == target) {
+            cout << "yes: indices are -> "
+                 << vec[left].second << " and " << vec[right].second << endl;
+            return;
+        } else if (current_sum < target) {
+            left++;
+        } else {
+            right--;
+        }
     }
 
     cout << "no" << endl;
@@ -52,4 +83,5 @@ int main(){
     int num=11;
     two_sum1(arr,num);
     two_sum2(arr,num);
+    two_sum3(arr,num);
 }
