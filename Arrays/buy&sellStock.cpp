fc79307166a62cf8ffcb326c6e35cb899e39bfff
vip23anchib <barman23vipanchi@gmail.com>
2025-12-30 18:29:51 +0000

    buy & sell stock

---
+++ Arrays/buy&sellStock.cpp
@@ -0,0 +1,19 @@
+// find the maximum profit from buying and selling a stock
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+    for (int i = 0; i < prices.size(); i++) {
+        minPrice = min(minPrice, prices[i]);
+        maxProfit = max(maxProfit, prices[i] - minPrice);
+    }
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "Maximum profit: " << maxProfit(prices) << endl;
+    return 0;
+}
