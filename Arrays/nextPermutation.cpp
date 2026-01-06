f9bbc047d77ea740d15afeac16b969064e5a3cdc
vip23anchib <barman23vipanchi@gmail.com>
2026-01-06 18:30:33 +0000

    next permutation

--- Arrays/nextPermutation.cpp
+++ Arrays/nextPermutation.cpp
@@ -6,29 +6,40 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+void nextPermutation(vector<int>& nums) {
+    int index = -1;
+    int n = nums.size();
 
-    void nextPermutation(vector<int>& nums) {
-        int index=-1;
-        int n=nums.size();
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index=i;
-                break;
-            }
+    // Step 1: Find the first index from the right where nums[i] < nums[i+1]
+    // This identifies the "break point" where the next permutation change can happen
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            index = i;
+            break;
         }
-        if (index==-1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        for(int i=n-1;i>index;i--){
-            if(nums[i]>nums[index]){
-                swap(nums[i],nums[index]);
-                break;
-            }
-        }
-        reverse(nums.begin()+index+1,nums.end());
-        
     }
+
+    // Step 2: If no such index exists, the array is in descending order
+    // This means we are at the last permutation
+    // Reverse the entire array to get the first permutation
+    if (index == -1) {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    // Step 3: Find the smallest element greater than nums[index] on the right side
+    // Swap it with nums[index] to get the next larger permutation
+    for (int i = n - 1; i > index; i--) {
+        if (nums[i] > nums[index]) {
+            swap(nums[i], nums[index]);
+            break;
+        }
+    }
+
+    // Step 4: Reverse the subarray to the right of index
+    // This makes the suffix as small as possible (lexicographically)
+    reverse(nums.begin() + index + 1, nums.end());
+}
 
     int main() {
         vector<int> nums = {1, 2, 3};
