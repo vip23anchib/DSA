7a2f84abf8029572251434a534d91f4787e77058
vip23anchib <barman23vipanchi@gmail.com>
2026-01-12 18:30:57 +0000

    set matrix

---
+++ Arrays/setMatrixZeroes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1;
+
+        // Step 1: mark rows and columns
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 2: update the matrix (excluding first row & col)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: handle first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: handle first column
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
