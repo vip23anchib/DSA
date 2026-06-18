0c1d7d3dd4fe6beec9d33bb34110e6e4d5549e44
vip23anchib <barman23vipanchi@gmail.com>
2026-06-18 16:28:22 +0530

    Refactor setZeroes method for improved clarity

--- Arrays/setMatrixZeroes.cpp
+++ Arrays/setMatrixZeroes.cpp
@@ -1,43 +1,51 @@
+//73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+
+//method 1: brute force - iterate 
 #include <bits/stdc++.h>
 using namespace std;
 
-void markRow(int i, vector<vector<int>>& matrix) {
-    for (int j = 0; j < matrix[0].size(); j++) {
-        if (matrix[i][j] != 0)
-            matrix[i][j] = -1;
-    }
-}
+class Solution {
+public:
+     // Function to set entire row and column to 0 if an element in the matrix is 0
+    void setZeroes(vector<vector<int>>& matrix) {
+        // Get number of rows
+        int m = matrix.size();
+        // Get number of columns
+        int n = matrix[0].size();
 
-void markCol(int j, vector<vector<int>>& matrix) {
-    for (int i = 0; i < matrix.size(); i++) {
-        if (matrix[i][j] != 0)
-            matrix[i][j] = -1;
-    }
-}
+        // Traverse each cell of the matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If current cell is zero
+                if (matrix[i][j] == 0) {
+                    // Mark all elements in this row as -1 (except existing zeros)
+                    for (int col = 0; col < n; col++) {
+                        if (matrix[i][col] != 0)
+                            matrix[i][col] = -1;
+                    }
+                    // Mark all elements in this column as -1 (except existing zeros)
+                    for (int row = 0; row < m; row++) {
+                        if (matrix[row][j] != 0)
+                            matrix[row][j] = -1;
+                    }
+                }
+            }
+        }
 
-void setZeroes(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                markRow(i, matrix);
-                markCol(j, matrix);
+        // Second pass: replace all -1 markers with 0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 0;
             }
         }
     }
-
-    // Final pass
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == -1)
-                matrix[i][j] = 0;
-        }
-    }
-}
+};
 
 
+// BETTER SOLUTION : keep extra row and column array and mark it 
 void betterSolution(vector<vector<int>>& matrix) {
     int n = matrix.size();
     int m = matrix[0].size();
